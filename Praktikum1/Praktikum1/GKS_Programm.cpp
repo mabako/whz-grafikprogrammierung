b9db1f8bed13735d2ecc4869c9be557dc9141cd5
Marcus Bauer <mabako@gmail.com>
2013-03-19 11:55:12 +0100

    Anfangsstand

---
+++ Praktikum1/Praktikum1/GKS_Programm.cpp
@@ -0,0 +1,35 @@
+#include "StdAfx.h"
+#include "GKS_Programm.h"
+
+extern CServer gs;
+
+CGKS_Programm::CGKS_Programm(void)
+{
+}
+
+CGKS_Programm::~CGKS_Programm(void)
+{
+}
+
+void CGKS_Programm::execute()
+{
+	AfxMessageBox("Programmende");
+}
+
+void CGKS_Programm::startGKS(void)
+{
+	// Server starten	
+	gs.gopen_gks("con");
+	gs.gopen_ws(1,NULL,GKS_SUBWINDOW);
+	gs.gactivate_ws(1);
+	gs.gx_set_win(1,0,400,0,300);
+	gs.gx_set_vp(1,0,1,0.1,0.85);
+}
+
+void CGKS_Programm::stopGKS(void)
+{
+	gs.gdeactivate_ws(1);
+	gs.gclose_ws(1);
+	// Server beenden
+	gs.gclose_gks();
+}
---
+++ Praktikum1/Praktikum1/GKS_Programm.h
@@ -0,0 +1,11 @@
+#pragma once
+
+class CGKS_Programm
+{
+public:
+	CGKS_Programm(void);
+	~CGKS_Programm(void);
+	void execute();
+	void startGKS();
+	void stopGKS();
+};
